444d5656352f91a12136e1d6bad52154cd9708cb
Epix <68203000+EpixScripts@users.noreply.github.com>
2025-05-04 12:45:26 -0700

    Add TODOs to instrument.rs

--- pumpkin-registry/src/instrument.rs
+++ pumpkin-registry/src/instrument.rs
@@ -2,8 +2,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Instrument {
-    sound_event: String,
-    use_duration: f32,
-    range: f32,
+    sound_event: String, // TODO: SoundEvent
+    use_duration: f32, // TODO: enforce PositiveFloat
+    range: f32, // TODO: enforce PositiveFloat
+    // TODO: is TextComponent ready?
     //  description: TextComponent<'static>,
 }
